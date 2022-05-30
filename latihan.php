996e65bba52efb9f49f826d133857ad879816f24
ferri <ferrisaputro99@gmailo.com>
2022-05-30 08:59:19 +0700

    menambahkan latihan file baru

---
+++ latihan.php
@@ -0,0 +1,22 @@
+<?php
+echo"Hello Worldd";
+$nama = "Kira";
+var_dump($nama);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+    
+
+    
+    
+</body>
+</html>
